a89b461b27c1dd0374db878e6a1c2e99038179ad
hahuuhoang73 <110458374+hahuuhoang73@users.noreply.github.com>
2023-04-18 19:28:33 +0700

    baitapvenha

--- T2207A/Program.cs
+++ T2207A/Program.cs
@@ -5,79 +5,82 @@ public class Program
 {
     public static void Main(String[] args)
     {
-       // Console.WriteLine("Hello World");
-       // int x = 10;
-       // string s = "T2207a";
-       // double pi = 3.14159;
-       // if (x > 5)
-       // {
-       //     x--;
-       // }
-      //  else
-      //  {
+        // Console.WriteLine("Hello World");
+        // int x = 10;
+        // string s = "T2207a";
+        // double pi = 3.14159;
+        // if (x > 5)
+        // {
+        //     x--;
+        // }
+        //  else
+        //  {
         //    x++;
-      //  }
-       // int[] arr = new int[5];
-      //  arr[0] = 1;
-      //  arr[1] = 56;
-      //  arr[2] = 12;
-      //  arr[3] = 2;
-       // arr[4] = 4;
-        
-       // foreach( int i in arr)
-       // {
-       //     Console.WriteLine(i);
-       // }
-      //  Console.WriteLine("Vui long nhap");
-      //  String cmd = Console.ReadLine();
-      //  Console.WriteLine("Chuoi vua nhap: " + cmd);
+        //  }
+        // int[] arr = new int[5];
+        //  arr[0] = 1;
+        //  arr[1] = 56;
+        //  arr[2] = 12;
+        //  arr[3] = 2;
+        // arr[4] = 4;
 
-      //  Console.WriteLine("vui long nhap so can kiem tra");
-      //  String cmd2 = Console.ReadLine();
-       // int n = Convert.ToInt32(cmd2);
-      //  if (isPrimed(n))
-       // {
-       //     Console.WriteLine(n + "la SNT");
-      //  }
+        // foreach( int i in arr)
+        // {
+        //     Console.WriteLine(i);
+        // }
+        //  Console.WriteLine("Vui long nhap");
+        //  String cmd = Console.ReadLine();
+        //  Console.WriteLine("Chuoi vua nhap: " + cmd);
 
-      //  List<int> ints = new List<int>();
-      //  Class1 cl = new Class1();
-      //  cl.Run();
-       // cl.Learn();
+        //  Console.WriteLine("vui long nhap so can kiem tra");
+        //  String cmd2 = Console.ReadLine();
+        // int n = Convert.ToInt32(cmd2);
+        //  if (isPrimed(n))
+        // {
+        //     Console.WriteLine(n + "la SNT");
+        //  }
+
+        //  List<int> ints = new List<int>();
+        // Class1 cl = new Class1();
+        //  cl.Run();
+        //  cl.Learn();
+        // cl.Learnn("abc");
+        //cl.Name = "Nguyen van an";
 
 
         //Bai toan phan so
-        Phanso ps = new Phanso();
-        ps.Nhap();
-        Console.WriteLine("Phan so vua nhap la:");
-        ps.Hienthi();
-        Console.WriteLine("Hay nhap phan so muon thuc hienj phep toan:");
-        Phanso psnew = new Phanso();
-        psnew.Nhap();
+        //Phanso ps = new Phanso();
+        //ps.Nhap();
+        //Console.WriteLine("Phan so vua nhap la:");
+        //ps.Hienthi();
+        //Console.WriteLine("Hay nhap phan so muon thuc hienj phep toan:");
+        //Phanso psnew = new Phanso();
+        //psnew.Nhap();
 
         //Giai quyet bai toan 
-        Console.WriteLine("tONG HAI PHAN SO LA");
-        Phanso tong = ps.Cong(psnew);
-        tong.Rutgonphanso();
-        tong.Hienthi();
+        //Console.WriteLine("tONG HAI PHAN SO LA");
+        //Phanso tong = ps.Cong(psnew);
+        //tong.Rutgonphanso();
+        //tong.Hienthi();
 
-        Console.WriteLine("TRU HAI PHAN SO LA");
-        Phanso tru = ps.Tru(psnew);
-        tru.Rutgonphanso();
-        tru.Hienthi();
+        //Console.WriteLine("TRU HAI PHAN SO LA");
+        //Phanso tru = ps.Tru(psnew);
+        //tru.Rutgonphanso();
+        //tru.Hienthi();
 
-        Console.WriteLine("NHAN HAI PHAN SO LA");
-        Phanso nhan = ps.Nhan(psnew);
-        nhan.Rutgonphanso();
-        nhan.Hienthi();
+        //Console.WriteLine("NHAN HAI PHAN SO LA");
+        //Phanso nhan = ps.Nhan(psnew);
+        //nhan.Rutgonphanso();
+        //nhan.Hienthi();
 
-        Console.WriteLine("CHIA HAI PHAN SO LA");
-        Phanso chia = ps.Cong(psnew);
-        chia.Rutgonphanso();
-        chia.Hienthi();
+        //Console.WriteLine("CHIA HAI PHAN SO LA");
+        //Phanso chia = ps.Cong(psnew);
+        //chia.Rutgonphanso();
+        //chia.Hienthi();
 
-
-
+        Vietnam vn = new Vietnam("K123","Ha Huu Hoang", new DateTime(2023, 4, 14), "kinh doanh",61,50);
+        
+        vn.Hienthi();
 
 
 
---
+++ T2207A/Vietnam.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    public class Vietnam
+    {
+        public string maKH;
+        public string hoTen;
+        public DateTime ngayRaDon;
+        public string doiTuongKH;
+        public double soLuongKW;
+        public double donGia;
+        public double thanhTien;
+
+        public Vietnam(string maKH, string hoTen, DateTime ngayRaDon, string doiTuongKH, double soLuongKW,double donGia)
+        {
+            this.maKH = maKH;
+            this.hoTen = hoTen;
+            this.ngayRaDon = ngayRaDon;
+            this.doiTuongKH = doiTuongKH;
+            this.soLuongKW = soLuongKW;
+            this.donGia = donGia;
+            TinhThanhTien();
+        }
+        public void TinhThanhTien()
+        {
+            double dinhMuc = 0;
+            switch (doiTuongKH)
+            {
+                case "sinh hoat":
+                    dinhMuc = 50;
+                    break;
+                case "kinh doanh":
+                    dinhMuc = 100;
+                    break;
+                case "san xuat":
+                    dinhMuc = 200;
+                    break;
+                default:
+                    break;
+            }
+
+            if (soLuongKW <= dinhMuc)
+            {
+                thanhTien = soLuongKW * donGia;
+            }
+            else
+            {
+                double giaVuotDinhMuc = 0;
+                if (soLuongKW > dinhMuc && soLuongKW <= 50)
+                {
+                    giaVuotDinhMuc = 1000;
+                }
+                else if (soLuongKW > 50 && soLuongKW <= 100)
+                {
+                    giaVuotDinhMuc = 1200;
+                }
+                else if (soLuongKW > 100 && soLuongKW <= 200)
+                {
+                    giaVuotDinhMuc = 1500;
+                }
+                else if (soLuongKW > 200)
+                {
+                    giaVuotDinhMuc = 2000;
+                }
+                thanhTien = dinhMuc * donGia + (soLuongKW - dinhMuc) * giaVuotDinhMuc;
+            }
+        }
+        public void Hienthi()
+        {
+            Console.WriteLine(maKH+"---"+hoTen+"----"+ngayRaDon+"____"+doiTuongKH+"----"+soLuongKW+"------"+donGia+"----"+thanhTien);
+        }
+    }
+    }
+
+
